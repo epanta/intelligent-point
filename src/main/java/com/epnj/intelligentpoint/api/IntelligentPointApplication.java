df3871926cda584965b52509bbcd97aa2f5e4960
edvaldo.panta <edvaldopanta@gmail.com>
2019-12-08 00:48:09 -0300

    Commit iniicial dos arquivos do projeto

---
+++ src/main/java/com/epnj/intelligentpoint/api/IntelligentPointApplication.java
@@ -0,0 +1,13 @@
+package com.epnj.intelligentpoint.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epnj/intelligentpoint/api/IntelligentPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epnj.intelligentpoint.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
